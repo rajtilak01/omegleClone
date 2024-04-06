761844c828c5fed4679d96e6939d40a2b093f4fb
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-04-06 23:09:41 +0530

    backend setup

---
+++ frontend/src/components/Landing.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import { Link } from "react-router-dom";
+
+function Landing() {
+    const [name, setName ] = useState("");
+
+
+  return (
+
+    <div>
+        <input type="text"  onChange={(e) => {
+            setName(e.target.value);
+        }}>
+
+        </input>
+
+        <Link to={`/room/?name=${name}`}>
+            Join
+        </Link>
+    </div>
+  )
+}
+
+export default Landing
---
+++ frontend/src/components/Room.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom"
+
+function Room() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const name = searchParams.get('name');
+
+    useEffect(() => {
+        
+    }, [name]);
+  return (
+    <div>Hi {name}</div>
+  )
+}
+
+export default Room
